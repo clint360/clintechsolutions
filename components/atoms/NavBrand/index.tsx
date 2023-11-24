ae8e92eb18fa53ed6cc85ac8801ed14dde354801
Clint Animbom <clint360.rebase@gmail.com>
2023-11-24 01:04:32 +0100

    pushing to a fresh new public repository

---
+++ components/atoms/NavBrand/index.tsx
@@ -0,0 +1,34 @@
+import useMobileDeviceDetection from 'hooks/useMobileDetection'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const NavBrand = () => {
+  const isMobile = useMobileDeviceDetection()
+  return (
+    <>
+      <div
+        className={`select-none ${
+          isMobile ? 'cursor-default' : 'cursor-pointer'
+        }`}
+      >
+        <Link href={'/'} passHref>
+          <div className="w-fit h-fit">
+            {/* {<Image src={'/images/brand.svg'} width='135px' height={30} alt='company brand' loading='lazy' />} */}
+            <div
+            style={{
+              fontSize: "1.6rem",
+              color: "white",
+              fontWeight: "bold",
+              display: "flex"
+            }}
+            >Clin<span style={{color: "green"}}>Tech</span>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default NavBrand
